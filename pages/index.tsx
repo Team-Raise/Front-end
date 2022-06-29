0ea8ca0c1e380094e7fee01c11f4a62ebd2e42be
jinhyo-dev <jinhyo0850@gmail.com>
2022-06-29 21:48:29 +0900

    REALLY REALLY DONE

--- pages/index.tsx
+++ pages/index.tsx
@@ -32,62 +32,81 @@ const Home = () => {
   } else if (!data) {
     return <Loading/>
   } else {
-    return (
-      <>
-        <Nav/>
+    if (data.values.length === 0) {
+      return (
+        <>
+          <Nav/>
 
-        <Image
-          src={'/images/logo.png'}
-          width={315}
-          height={190}
-          alt={'Logo'}
-        />
+          <Image
+            src={'/images/logo.png'}
+            width={315}
+            height={190}
+            alt={'Logo'}
+          />
 
-        {Object.values(data.values).map((log: any) => (
-          <div key={1}>
+          <h1 className={cs('notExist')}>현재 입력받은 데이터가 존재하지 않습니다.</h1>
+        </>
+      )
 
-            <div className={cs('boxContainer')}>
+    } else {
+      return (
+        <>
+          <Nav/>
 
-              <div className={cs('humiBox', 'box')}>
-                <div className={cs('boxTitle', 'humi')}>습도</div>
-                <div className={cs('supplement')}>{log.humidity}%</div>
+          <Image
+            src={'/images/logo.png'}
+            width={315}
+            height={190}
+            alt={'Logo'}
+          />
+
+          {Object.values(data.values).map((log: any) => (
+            <div key={1}>
+
+              <div className={cs('boxContainer')}>
+
+                <div className={cs('humiBox', 'box')}>
+                  <div className={cs('boxTitle', 'humi')}>습도</div>
+                  <div className={cs('supplement')}>{log.humidity}%</div>
+                </div>
+
+                <div className={cs('humiBox', 'box', 'rightBox')}>
+                  <div className={cs('boxTitle', 'temp')}>온도</div>
+                  <div className={cs('supplement')}>{log.temperature}℃</div>
+                </div>
+
+                <div className={cs('humiBox', 'box', 'rightBox')}>
+                  <div className={cs('boxTitle', 'feelTemp')}>수위</div>
+                  <div className={cs('supplement')}>{log.water_level}</div>
+                </div>
+
+                <div className={cs('humiBox', 'box', 'rightBox')}>
+                  <div className={cs('boxTitle', 'ph')}>pH</div>
+                  <div className={cs('supplement')}>{log.ph}</div>
+                </div>
+
               </div>
-
-              <div className={cs('humiBox', 'box', 'rightBox')}>
-                <div className={cs('boxTitle', 'temp')}>온도</div>
-                <div className={cs('supplement')}>{log.temperature}℃</div>
+              <br/>
+              <div className={cs('updatedTime')}>최신 업데이트
+                <div style={{color: '#f00'}}>{log.date}</div>
               </div>
-
-              <div className={cs('humiBox', 'box', 'rightBox')}>
-                <div className={cs('boxTitle', 'feelTemp')}>수위</div>
-                <div className={cs('supplement')}>{log.water_level}</div>
-              </div>
-
-              <div className={cs('humiBox', 'box', 'rightBox')}>
-                <div className={cs('boxTitle', 'ph')}>pH</div>
-                <div className={cs('supplement')}>{log.ph}</div>
-              </div>
-
             </div>
-            <br/>
-            <div className={cs('updatedTime')}>최신 업데이트
-              <div style={{color: '#f00'}}>{log.date}</div>
-            </div>
+          ))}
+
+          <FancyModal/>
+          <br/>
+          <button className={cs('deleteButton')} onClick={onRemove}>
+            <AiOutlineDelete/> 데이터 초기화
+          </button>
+
+          <div className="footer">
+            <a href={'https://github.com/jinhyo-dev/Capstone-Project'} target={"blank"} className='footerFont'>© 2022
+              GBSW
+              Team. Raise</a>
           </div>
-        ))}
-
-        <FancyModal/>
-        <br/>
-        <button className={cs('deleteButton')} onClick={onRemove}>
-          <AiOutlineDelete/> 데이터 초기화
-        </button>
-
-        <div className="footer">
-          <a href={'https://github.com/jinhyo-dev/Capstone-Project'} target={"blank"} className='footerFont'>© 2022 GBSW
-            Team. Raise</a>
-        </div>
-      </>
-    )
+        </>
+      )
+    }
   }
 }
 
